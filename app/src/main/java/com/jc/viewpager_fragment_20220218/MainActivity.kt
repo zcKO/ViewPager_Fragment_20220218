01a3235cc521e95d876071db3f242c206e5d2732
JaeCheon <studyJcHub@gmail.com>
2022-02-18 16:36:15 +0900

    Initial commit

---
+++ app/src/main/java/com/jc/viewpager_fragment_20220218/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.viewpager_fragment_20220218
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
