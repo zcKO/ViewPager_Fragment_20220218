2225983ededaa33913b924ba2c68d916a4d67fb4
JaeCheon <studyJcHub@gmail.com>
2022-02-18 17:01:19 +0900

    필수로 구현 함수 2개 완성  - 총 몇 장짜리인지  - 각 position 에 어떤 프래그먼트가 나가는지

--- app/src/main/java/com/jc/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/jc/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -1,9 +1,29 @@
 package com.jc.viewpager_fragment_20220218.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.jc.viewpager_fragment_20220218.fragments.BirthFragment
+import com.jc.viewpager_fragment_20220218.fragments.HelloFragment
+import com.jc.viewpager_fragment_20220218.fragments.NameFragment
 
 class MainViewPagerAdapter(
     fm: FragmentManager
-): FragmentPagerAdapter(fm) {
+) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+        if (position == 0) {
+            return NameFragment()
+        } else if (position == 1) {
+            return BirthFragment()
+        } else {
+            return HelloFragment()
+        }
+
+    }
 }
