e5aac6e398e40d20979bb6341b5754cbee6145f5
JaeCheon <studyJcHub@gmail.com>
2022-02-18 16:51:52 +0900

    생성자를 수정하여 FragmentManager 를 받아서 부모 생성자에 전달

--- app/src/main/java/com/jc/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/jc/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -1,6 +1,9 @@
 package com.jc.viewpager_fragment_20220218.adapters
 
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class MainViewPagerAdapter: FragmentPagerAdapter() {
+class MainViewPagerAdapter(
+    fm: FragmentManager
+): FragmentPagerAdapter(fm) {
 }
